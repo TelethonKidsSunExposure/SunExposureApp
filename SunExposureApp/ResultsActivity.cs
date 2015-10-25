f58e76d73c8cd37adcdce6598f7f5807a8e964c8
Macklin Hartley <macklin@outlook.com>
2015-10-25 11:28:45 +0800

    Scrollview on results page

--- SunExposureApp/ResultsActivity.cs
+++ SunExposureApp/ResultsActivity.cs
@@ -15,7 +15,8 @@ using Android.Graphics;
 
 namespace SunExposureApp
 {
-	[Activity (Label = "ResultsActivity")]			
+	[Activity (Label = "ResultsActivity",
+        Theme = "@android:style/Theme.DeviceDefault.Light.NoActionBar")]			
 	public class ResultsActivity : Activity, Android.Views.View.IOnClickListener
 	{
 		protected override void OnCreate (Bundle bundle)
@@ -26,10 +27,6 @@ namespace SunExposureApp
 
 			// Create your application here
 			var buttons = new List<Button> {FindViewById<Button>(Resource.Id.saveProfile), FindViewById<Button> (Resource.Id.setReminder)};
-
-			var lightingColorFilter = new LightingColorFilter (0xFFFFFFFF, 0xFF1A1D30);
-			buttons.ForEach (x => x.Background.SetColorFilter (lightingColorFilter));
-			buttons.ForEach (x => x.SetOnClickListener (this));
 		}
 
 		public void OnClick (View v)
